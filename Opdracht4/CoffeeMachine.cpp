1e29c245927d06693a23a05711e50954599c8f2c
narasu <narasu@users.noreply.github.com>
2020-10-11 15:31:55 +0200

    added coin names

--- Opdracht4/CoffeeMachine.cpp
+++ Opdracht4/CoffeeMachine.cpp
@@ -4,12 +4,13 @@
 
 void CoffeeMachine::insertCoin(Coin c)
 {
-	
 	currentCoin = c.getType();
+	
 }
 
 void CoffeeMachine::showCoffee()
 {
+	std::cout << "You have inserted a " << coinName[currentCoin] << "coin. " << std::endl;
 	std::cout << "You may buy the following coffee:" << std::endl;
 	
 	for (int i = 0; i <= currentCoin; i++)
--- Opdracht4/CoffeeMachine.h
+++ Opdracht4/CoffeeMachine.h
@@ -7,6 +7,7 @@ class CoffeeMachine
 {
 private:
 	int currentCoin = -1;
+	std::string coinName[3] = { "Fifty cents", "One euro", "Special" };
 	std::string coffee[3] = { "Espresso", "Cappuccino", "Magic Coffee" };
 public:
 	void insertCoin(Coin c);
